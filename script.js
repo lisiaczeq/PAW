a87f452b65485f84099d75d5f2f757eb8acce492
Mateusz Lisowski <84307013+lisiaczeq@users.noreply.github.com>
2025-10-07 08:06:55 +0000

    1_1.2

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-(function() {
-  ex1_button.onclick = function() {
-    var tabela = []
+  (function() {
+    ex1_button.onclick = function() {
+      var tabela = [];
 
-    for (var i = 0; i <= 9; i++) {
+      for (var i = 0; i <= 9; i++) {
+        tabela.push(i);
+      }
 
-      tabela.push(i);
+      ex1_content.innerHTML = tabela.toString();
+    };
 
-    }
+    document.addEventListener('DOMContentLoaded', function () {
+      const input = document.getElementById('ex2_text');
+      const content = document.getElementById('ex2_content');
 
-    ex1_content.innerHTML = tabela.toString();
+      function validatePhoneNumber(value) {
+        if (value.length !== 9) {
+          return "Długość numeru musi być równa 9";
+        }
+        if (/[a-zA-Z]/.test(value)) {
+          return "Numer nie może zawierać liter";
+        }
+        if (/[^0-9]/.test(value)) {
+          return "Numer nie może zawierać znaków specjalnych";
+        }
+        return "Numer telefonu jest poprawny";
+      }
 
-  }
-})();
+      input.addEventListener('input', function () {
+        const msg = validatePhoneNumber(input.value);
+        content.textContent = msg;
+      });
+    });
+  })();
