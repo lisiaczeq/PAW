21a9fea0e468ca3ef91bf4634221f24379e22d1c
Mateusz Lisowski <84307013+lisiaczeq@users.noreply.github.com>
2025-10-07 07:58:11 +0000

    1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-(function () {
-  //TODO
+(function() {
+  ex1_button.onclick = function() {
+    var tabela = []
+
+    for (var i = 0; i <= 9; i++) {
+
+      tabela.push(i);
+
+    }
+
+    ex1_content.innerHTML = tabela.toString();
+
+  }
 })();
