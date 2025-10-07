ffece021d8aac153c9865f2c7421491486d4104a
Mateusz Lisowski <84307013+lisiaczeq@users.noreply.github.com>
2025-10-07 07:51:52 +0000

    Rollback

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 (function () {
-  ex1_button.onclick = function() {
-    var tabela = [];
-    for(var i = 0; i <= 9; i++) {
-      tabela.push(i);
-    }
-    ex1_content.innerHTML = tabela.toString();
-  }
+  //TODO
 })();
